ff54fa7ccd06df207adfc125710d1964a8e34c0f
Farmeurimmo <farmeurimmo@gmail.com>
2023-03-20 19:09:23 +0100

    Fix /discord et /mumble

--- src/main/java/fr/opaleuhc/opalevelocity/cmd/DiscordCmd.java
+++ src/main/java/fr/opaleuhc/opalevelocity/cmd/DiscordCmd.java
@@ -3,9 +3,13 @@ package fr.opaleuhc.opalevelocity.cmd;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ServerConnection;
+import net.kyori.adventure.inventory.Book;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public class DiscordCmd implements SimpleCommand {
@@ -19,7 +23,10 @@ public class DiscordCmd implements SimpleCommand {
             source.sendMessage(Component.text("§cVous devez être un joueur pour exécuter cette commande !"));
             return;
         }
-        p.sendMessage(Component.text("§3Discord : §bhttps://discord.opaleuhc.fr"));
+        Component component = Component.text("\n§3Discord : §bhttps://discord.opaleuhc.fr\n");
+        component = component.hoverEvent(Component.text("§bCliquez ici pour rejoindre le discord !"));
+        component = component.clickEvent(ClickEvent.openUrl("https://discord.opaleuhc.fr"));
+        p.sendMessage(component);
     }
 
     @Override
--- src/main/java/fr/opaleuhc/opalevelocity/cmd/MumbleCmd.java
+++ src/main/java/fr/opaleuhc/opalevelocity/cmd/MumbleCmd.java
@@ -4,6 +4,7 @@ import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.proxy.Player;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -19,7 +20,10 @@ public class MumbleCmd implements SimpleCommand {
             source.sendMessage(Component.text("§cVous devez être un joueur pour exécuter cette commande !"));
             return;
         }
-        p.sendMessage(Component.text("§3Mumble : §bhttps://mumble.opaleuhc.fr"));
+        Component component = Component.text("\n§3Addresse du mumble : §bhttps://mumble.opaleuhc.fr\n");
+        component = component.hoverEvent(Component.text("§bCliquez ici pour rejoindre le discord !"));
+        component = component.clickEvent(ClickEvent.openUrl("https://mumble.opaleuhc.fr"));
+        p.sendMessage(component);
     }
 
     @Override

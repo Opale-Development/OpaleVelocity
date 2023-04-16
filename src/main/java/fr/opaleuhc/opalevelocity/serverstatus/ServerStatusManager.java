efd289150276f5be4d8c91ebd1674bb008d315ef
Farmeurimmo <farmeurimmo@gmail.com>
2023-04-16 14:08:55 +0200

    Ajout envoi du nombre de connecté si le serveur est allumé

--- src/main/java/fr/opaleuhc/opalevelocity/serverstatus/ServerStatusManager.java
+++ src/main/java/fr/opaleuhc/opalevelocity/serverstatus/ServerStatusManager.java
@@ -18,8 +18,9 @@ public class ServerStatusManager {
     public static final long NO_PING_BEFORE_GOING_OFF = 5000;
     public static final MinecraftChannelIdentifier STATUT_CHANNEL = MinecraftChannelIdentifier.from("opaleuhc:srvstatus");
     public static ServerStatusManager INSTANCE;
-    private HashMap<String, ServerStatus> serversStatus = new HashMap<>();
-    private HashMap<String, Long> lastPing = new HashMap<>();
+    private final HashMap<String, ServerStatus> serversStatus = new HashMap<>();
+    private final HashMap<String, Long> lastPing = new HashMap<>();
+    private final HashMap<String, Integer> playersCount = new HashMap<>();
 
     public ServerStatusManager() {
         INSTANCE = this;
@@ -35,17 +36,24 @@ public class ServerStatusManager {
     public void pingServers() {
         CompletableFuture.runAsync(() -> {
             for (RegisteredServer rs : OpaleVelocity.instance.getProxy().getAllServers()) {
+                final int pOnline = rs.getPlayersConnected().size();
                 rs.ping().whenComplete((ping, throwable) -> {
                     if (throwable != null) {
                         serversStatus.put(rs.getServerInfo().getName(), ServerStatus.OFF);
                         lastPing.put(rs.getServerInfo().getName(), System.currentTimeMillis());
+                        playersCount.put(rs.getServerInfo().getName(), -1);
                     } else {
                         serversStatus.put(rs.getServerInfo().getName(), ServerStatus.ON);
                         lastPing.put(rs.getServerInfo().getName(), System.currentTimeMillis());
+                        playersCount.put(rs.getServerInfo().getName(), pOnline);
                     }
                 });
             }
         });
+    }
+
+    public int getPlayerCount(String serverName) {
+        return playersCount.getOrDefault(serverName, -1);
     }
 
     public boolean isServerOnline(String serverName) {
@@ -59,9 +67,13 @@ public class ServerStatusManager {
     public void sendStatusForEveryServers() {
         CompletableFuture.runAsync(() -> lastPing.keySet().forEach(serverName -> {
             for (Player player : OpaleVelocity.instance.getProxy().getAllPlayers()) {
+                if (player.getCurrentServer().isEmpty()) continue;
+                if (player.getCurrentServer().get().getServer().getServerInfo().getName().equals(serverName)) continue;
                 ByteArrayDataOutput out = ByteStreams.newDataOutput();
                 out.writeUTF(serverName);
                 out.writeUTF(getServerStatus(serverName).name());
+                int playerCount = getPlayerCount(serverName);
+                if (playerCount != -1) out.writeUTF(String.valueOf(playerCount));
                 Optional<ServerConnection> serverConnection = player.getCurrentServer();
                 serverConnection.ifPresent(connection -> connection.sendPluginMessage(STATUT_CHANNEL, out.toByteArray()));
                 break;
